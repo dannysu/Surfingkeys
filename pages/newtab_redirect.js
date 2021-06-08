ee5cfea5f2c0a89f34777b1f5964b62d6d5201cf
Danny Su <contact@dannysu.com>
2021-06-08 00:53:36 -0700

    Make newtab work with temporary container

--- pages/newtab_redirect.js
+++ pages/newtab_redirect.js
@@ -1,5 +1,10 @@
 browser.tabs.getCurrent(tab => {
-  const tabId = tab.id;
+  if (!tab.cookieStoreId || !tab.cookieStoreId.startsWith('firefox-container-')) {
+    browser.history.deleteUrl({ url: browser.runtime.getURL('pages/newtab_redirect.html') });
+    return;
+  }
+
+  var tabId = tab.id;
   browser.tabs.create({ url: browser.runtime.getURL('pages/newtab.html'), cookieStoreId: tab.cookieStoreId }, () => {
     browser.tabs.remove(tabId);
   });
